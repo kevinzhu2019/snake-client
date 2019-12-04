40e38192aa29a0c906b499374b921ecac17cb58e
kevinzhu2019 <kevinzhu.zyk@gmail.com>
2019-12-04 18:31:41 +0000

    separate the connection file with gaming file

--- play.js
+++ play.js
@@ -1,23 +1,4 @@
-const net = require('net');
-
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: '172.46.2.204',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
-  });
-
-  return conn;
-  
-}
-
+// play.js
+const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
